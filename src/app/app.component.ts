dca7898cef75bd8203e5861edbfac86d4440abb7
Vlad Tancev <vlad.tancev@gmail.com>
2022-09-18 19:58:54 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {products as data} from "./data/products";
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-crash-course';
+export class AppComponent implements OnInit {
+  constructor(private productsService: ProductsService) {
+  }
+  title = 'angular app';
+  loading = false;
+  // products: IProduct[] = [];
+  products$!: Observable<IProduct[]>;
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService.getAll().pipe(
+      tap( () => this.loading = false)
+    );
+  }
 }
